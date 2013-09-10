3a7724ba0adde493f086cf0dac3bbc1ba7bc50c4
Mike Harris <mharris717@gmail.com>
2013-09-10 12:19:05 -0400

    add depends to metadata

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,10 @@ license          'All rights reserved'
 description      'Installs/Configures mharris717'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
+
+depends "git"
+depends 'apt'
+depends 'nginx'
+depends 'postgresql'
+depends 'npm'
+depends 'unicorn'
